3cc1a0a2fbb130ac6e9e93232fd6ac3b08b8bae4
AndrewMPatterson <Ampatte2@asu.edu>
2025-03-31 13:12:39 -0700

    Update keymap.lua

--- linux-config/nvim/lua/keymap.lua
+++ linux-config/nvim/lua/keymap.lua
@@ -4,4 +4,8 @@ vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>l', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>le', builtin.buffers, {})
 vim.keymap.set('n', '<leader>lf', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>g', ':Ex<CR>', {})
+vim.keymap.set('n', '<leader>n', ':bd<CR>', {})
+vim.keymap.set('n', '<leader>e', vim.lsp.buf.hover, {})
+vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, {})
 
