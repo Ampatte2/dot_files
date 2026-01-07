f294d3b08143a6aeb3f9622acaedbb21e4d27104
Andrew Patterson <ampatte2@asu.edu>
2026-01-07 13:39:57 -0700

    Dynamic-Window-Storage

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,32 +1,44 @@
 Critty="Alacritty"
 Browser=os.getenv("HAMMERSPOON-BROWSER") or "Google Chrome"
 ITerm="iTerm"
-CachedWindow=nil
+VSCode="Visual Studio Code"
 
-hs.hotkey.bind({"cmd", "ctrl"}, "K", function()
-    hs.application.open(ITerm)
-end)
+function createLayoutBinding (keyCode, reference)
+    hs.hotkey.bind({"cmd", "ctrl", "shift"}, keyCode, function()
+        hs.alert(reference == nil)
+        if (reference == nil) then reference = hs.window.frontmostWindow() else reference = nil end
+    end)
 
-hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
-    hs.application.open(Critty)
-end)
+    hs.hotkey.bind({"cmd", "ctrl"}, keyCode, function()
+        if not (reference == nil) then reference:focus() else hs.alert.show("No Cached Window") end
+    end)
+end    
 
-hs.hotkey.bind({"cmd", "ctrl"}, "H", function()
-    hs.application.open(Browser)
-end)
+CachedWindowK=nil
+createLayoutBinding("K", CachedWindowK)
 
-hs.hotkey.bind({"cmd", "ctrl", "shift"}, "J", function()
-    if (CachedWindow == nil) then CachedWindow = hs.window.frontmostWindow() else CachedWindow = nil end
-end)
+CachedWindowL=nil
+createLayoutBinding("L", CachedWindowL)
 
-hs.hotkey.bind({"cmd", "ctrl"}, "J", function()
-    if not (CachedWindow == nil) then CachedWindow:focus() else hs.alert.show("No Cached Window") end
-end)
+CachedWindowH=nil
+createLayoutBinding("H", CachedWindowH)
+
+CachedWindowJ=nil
+createLayoutBinding("J", CachedWindowJ)
+
+CachedWindowO=nil
+createLayoutBinding("O", CachedWindowO)
+
+CachedWindowE=nil
+createLayoutBinding("E", CachedWindowE)
+
+CachedWindowG=nil
+createLayoutBinding("G", CachedWindowG)
 
 local laptopScreen = os.getenv("HAMMERSPOON-DISPLAY-NAME") or "C32R50x";
 local windowLayout = {
     {Browser,  nil, laptopScreen, hs.layout.left50,    nil, nil},
-    {Critty,   nil,          laptopScreen, hs.layout.right50,   nil, nil},
+    {Critty,   nil, laptopScreen, hs.layout.right50,   nil, nil},
 }
 
 hs.hotkey.bind({"cmd", "ctrl"}, "I", function()
